e0ab5a967226e1974043aac142cebfb60dded8b3
SeherKumsar <seherkumsar99@outlook.com>
2024-03-14 20:21:32 +0300

    In the thunk, make the request, return the data that you want to use in the reducer

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -1,6 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const fetchUsers = createAsyncThunk('users/fetch');
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+  const response = await axios.get('http://localhost:3005/users');
+
+  
+
+  return response.data;
+});
+
 
 
 export { fetchUsers };
