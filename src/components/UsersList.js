ee0964868a44d8a5ce6d1600bbe3b8ec97273805
SeherKumsar <seherkumsar99@outlook.com>
2024-03-15 00:54:04 +0300

    we need data for useSelector

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,18 +1,19 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
 
 function UsersList() {
   const dispatch = useDispatch();
+  useSelector((state) => {
+    return state.users; // { data: [], isLoading: false, error: null}
+  });
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-
-
-
-  return "Users Lists"
+  
+  return 'Users List';
 }
 
 export default UsersList;
