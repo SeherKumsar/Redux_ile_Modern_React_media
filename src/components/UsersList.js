bdf49cdb8dd709e1d36bd1ff5973a177e2f42632
SeherKumsar <seherkumsar99@outlook.com>
2024-03-15 01:38:55 +0300

    Animations with TailwindCSS

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -14,12 +14,13 @@ function UsersList() {
   }, [dispatch]);
 
   if (isLoading) {
-    return <Skeleton times={6} />;
+    return <Skeleton times={6} className="h-10 w-full" />;
   }
 
   if (error) {
     return <div>Error fetching data...</div>;
   }
+
 
   return <div>{data.length}</div>;
 }
