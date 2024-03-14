f1729d3a6d008306ee1c5bbd474757f8e511e488
SeherKumsar <seherkumsar99@outlook.com>
2024-03-15 00:50:13 +0300

    When a user does something, dispatch the thunk function to run it

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchUsers } from '../store';
+
 function UsersList() {
-    return 'Users List';
-  }
-  
-  export default UsersList;
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+
+
+
+  return "Users Lists"
+}
+
+export default UsersList;
