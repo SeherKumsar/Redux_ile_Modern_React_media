763c48c9c395f487e08a3b8e170d44c6b60c7ab4
SeherKumsar <seherkumsar99@outlook.com>
2024-03-14 20:44:43 +0300

    In the slice, add extraReducers, watching for the action types made by the thunk ( More on adding Thunks)

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers} from '../thunks/fetchUsers';
 
 const usersSlice = createSlice({
   name: "users",
@@ -7,7 +8,23 @@ const usersSlice = createSlice({
     isLoading: false,
     error: null,
   },
-  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      // Update our state object howerver appropriate
+      // to show the user what we are loading data
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = true;
+      state.data = action.payload;
+      //payload whatever action autcomplete side-effects we want to do here
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+      // When the request fails, an error object is automatically created for us
+    });
+  }
 });
 
 export const usersReducer = usersSlice.reducer;
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,11 +4,13 @@ import axios from 'axios';
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3005/users');
 
-  
-
   return response.data;
+  // return response whatever 
+  // you return here is going to be automatically assigned to action.payload
 });
 
-
+// fetchUsers.pending === 'users/fetch/pending'
+// fetchUsers.fulfilled === 'users/fetch/fulfilled'
+// fetchUsers.rejected === 'users/fetch/rejected'
 
 export { fetchUsers };
