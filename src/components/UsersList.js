3e4132429d5f531f0dc2afd201334b76d918ffba
SeherKumsar <seherkumsar99@outlook.com>
2024-03-15 10:46:07 +0300

    Refactoring the Component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,6 +4,7 @@ import { fetchUsers, addUser } from '../store';
 import Button from './Button';
 import Skeleton from './Skeleton';
 import { useThunk } from '../hooks/use-thunk';
+import UsersListItem from './UsersListItem';
 
 function UsersList() {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
@@ -21,14 +22,6 @@ function UsersList() {
     doCreateUser();
   };
 
-  // if (isLoadingUsers) {
-  //   return <Skeleton times={6} className="h-10 w-full" />;
-  // }
-
-  // if (loadingUsersError) {
-  //   return <div>Error fetching data...</div>;
-  // }
-
   let content;
   if (isLoadingUsers) {
     content = <Skeleton times={6} className="h-10 w-full" />;
@@ -36,38 +29,19 @@ function UsersList() {
     content = <div>Error fetching data...</div>;
   } else {
     content = data.map((user) => {
-      return (
-        <div key={user.id} className="mb-2 border rounded">
-          <div className="flex p-2 justify-between items-center cursor-pointer">
-            {user.name}
-          </div>
-        </div>
-      );
+      return <UsersListItem key={user.id} user={user} />;
     });
   }
-
-  // const renderedUsers = data.map((user) => {
-  //   return (
-  //     <div key={user.id} className="mb-2 border rounded">
-  //       <div className="flex p-2 justify-between items-center cursor-pointer">
-  //         {user.name}
-  //       </div>
-  //     </div>
-  //   );
-  // });
 
   return (
     <div>
       <div className="flex flex-row justify-between items-center m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        {isCreatingUser ? (
-          'Creating User...'
-        ) : (
-          <Button onClick={handleUserAdd}>+ Add User</Button>
-        )}
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>
+          + Add User
+        </Button>
         {creatingUserError && 'Error creating user...'}
       </div>
-      {/* {renderedUsers} */}
       {content}
     </div>
   );
