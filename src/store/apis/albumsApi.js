47bc678bb2a6d3e623892b7eebfece15dc757bdf
SeherKumsar <seherkumsar99@outlook.com>
2024-03-15 13:51:32 +0300

    Connect the API to the store. Reducer, middleware, and listeners

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,30 +1,27 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const albumsApi = createApi({
-  reducerPath: "albums",
   // We use fetch instead of axios for the API calls
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3005",
   }),
-
   // We use endpoints to define the API calls
   endpoints(builder) {
     return {
       fetchAlbums: builder.query({
         query: (user) => {
           return {
-            url: `/albums`,
+            url: '/albums',
             params: {
               userId: user.id,
             },
-            method: "GET",
+            method: 'GET',
           };
         },
       }),
     };
   },
 });
-// We use a custom hook to use the API
-// albumsApi.useGetAlbumsQuery();
+
 export const { useFetchAlbumsQuery } = albumsApi;
-export default albumsApi;
+export { albumsApi };
