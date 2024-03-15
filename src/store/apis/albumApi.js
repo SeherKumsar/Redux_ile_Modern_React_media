c62fa10fb6c947586f604d1258421639ee250007
SeherKumsar <seherkumsar99@outlook.com>
2024-03-15 12:15:22 +0300

    The API needs to know how and where to send requests. Add a 'baseQuery'

--- src/store/apis/albumApi.js
+++ src/store/apis/albumApi.js
@@ -1,6 +1,9 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const albumsApi = createApi({
     reducerPath: 'albums',
-    
+    // We use fetch instead of axios for the API calls
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: 'http://localhost:3005' 
+    }),
 });
