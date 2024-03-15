c051a8cced427d2c1c9e240de5557a470c8013b6
SeherKumsar <seherkumsar99@outlook.com>
2024-03-15 13:39:51 +0300

    Add 'endpoints', one for each kind of request you want to make. Reqs that read data are queries, write data are mutations

--- src/store/apis/albumApi.js
+++ src/store/apis/albumApi.js
@@ -1,9 +1,28 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const albumsApi = createApi({
-    reducerPath: 'albums',
-    // We use fetch instead of axios for the API calls
-    baseQuery: fetchBaseQuery({ 
-        baseUrl: 'http://localhost:3005' 
-    }),
+  reducerPath: "albums",
+  // We use fetch instead of axios for the API calls
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+
+  // We use endpoints to define the API calls
+  endpoints(builder) {
+    return {
+      fetchAlbums: builder.query({
+        query: (user) => {
+          return {
+            url: `/albums`,
+            params: {
+              userId: user.id,
+            },
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
 });
+// We use a custom hook to use the API
+// albumsApi.useGetAlbumsQuery();
