2a9261137ec078838719c9d8b31ef81c67a7f1e9
SeherKumsar <seherkumsar99@outlook.com>
2024-03-16 19:35:03 +0300

    Lots of Photos Setup!

---
+++ src/store/apis/photosApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const photosApi = createApi({
+  reducerPath: "photos",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+  endpoints(builder) {
+    return {
+      fetchPhotos: builder.query({}),
+      addPhoto: builder.query({}),
+      removePhoto: builder.query({}),
+    };
+  },
+});
+
+export const {
+  useFetchPhotosQuery,
+  useAddPhotoMutation,
+  useRemovePhotoMutation,
+} = photosApi;
+export { photosApi };
